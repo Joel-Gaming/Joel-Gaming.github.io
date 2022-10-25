80678eee4fa96a9b97af3d8bf0bc62991db1918d
Joel-Gaming <113944087+Joel-Gaming@users.noreply.github.com>
2022-10-25 12:17:37 -0600

    started on projject #2

--- Grid/sketch.js
+++ Grid/sketch.js
@@ -1,10 +1,9 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Basic Grid Demo
+// Joel Penner
 
+let grid = [[0, 0, 1, 0, 0], 
+  [0, 1, 0, 1, 0],
+  [1,  0, 0, 0, 1]];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,4 +11,36 @@ function setup() {
 
 function draw() {
   background(220);
+  displayGrid(grid);
+}
+
+function mousePressed() {
+  let cellWidth = width / grid[0].length;
+  let cellHeight = height / grid.length;
+
+  let x = Math.floor(mouseX/cellWidth);
+  let y = Math.floor(mouseY/cellHeight);
+
+  if (grid[y][x] === 0) {
+    grid[y][x] = 1;
+  }
+  else if (grid[y][x] === 1) {
+    grid[y][x] = 0;
+  }
+}
+
+function displayGrid(grid) {
+  let cellWidth = width / grid[0].length;
+  let cellHeight = height / grid.length;
+  for (let y=0; y<grid.length; y++) {
+    for (let x=0; x<grid[y].length; x++) {
+      if (grid[y][x] === 0) {
+        fill("white");
+      }
+      else if (grid[y][x] === 1) {
+        fill("black");
+      }
+      rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+    }
+  }
 }
---
+++ Project #2/sketch.js
@@ -0,0 +1,13 @@
+// 2 Dementanial Grid Assignment
+// Joel Penner
+// Date
+
+let grid = [[0, 0, 0, 3]]
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
