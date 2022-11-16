2575b9306995327f7af7f371646156575dac9eb1
Joel-Gaming <113944087+Joel-Gaming@users.noreply.github.com>
2022-11-16 12:17:02 -0600

    added demo

--- Bullet Demo/sketch.js
+++ Bullet Demo/sketch.js
@@ -4,7 +4,30 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
+class Bullet {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.bullet;
+    this.dx = random(-5, 5);
+    this.dy = random(-5,5);
+  } 
 
+  preload() {
+    this.bullet = loadImage ("bullet.png");
+  }
+  
+  display() {
+    image(this.bullet);
+  }
+
+  move() {
+    this.x += this.dx;
+    this.y += this.dy;
+  }
+}
+
+let theBullets = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,4 +35,8 @@ function setup() {
 
 function draw() {
   background(220);
+  for (let i = 0; i < theBullets.length; i++) {
+    theBullets[i].move();
+    theBullets.display();
+  }
 }
